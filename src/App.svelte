37ed88f80210910d4320be1cce0f05f85db8bdcb
Zeeshan Hooda <zeeshanhooda@icloud.com>
2021-02-04 12:07:25 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Prediction from './Prediction.svelte';
+	import { Container } from 'sveltestrap';
+</script>
+
+<Container class="pt-5">
+	<Prediction/>
+</Container>
---
+++ src/Prediction.svelte
@@ -0,0 +1,69 @@
+<script>
+import App from "./App.svelte";
+import { Col, Row, Button, Input, Form } from 'sveltestrap';
+
+	let src = {
+		verbose: 'type a message!',
+		prediction: '-10,000'
+	};
+	let text = '';
+	let submitable;
+	$: submitable = text.length > 0
+	
+	function getPrediction(t) {
+		src = {
+			prediction: ''
+		}
+		fetch(`http://144.202.25.44:6969/api/v1/predict?text=${text}&verbose=true`)
+			.then((response) => response.json())
+			.then(json => {
+				src = json
+			})
+		.catch(err => {
+			src = err
+		})
+	}
+
+	function handleEnter(e) {
+		if (e.keyCode === 13 && text.length > 0) {
+			getPrediction("b")
+		}
+	}
+</script>
+
+<style>
+	:global(.predict) {
+		margin-left: 0;
+		margin-right: 0;
+		width: 100%
+	}
+</style>
+
+<!-- <Form>	 -->
+	<Row>
+		<Col md='8' xs='8'>
+			<Input type="text" bind:value={text} on:submit={getPrediction(text)} on:keyup={handleEnter} placeholder="bonkers"/>
+		</Col>
+		<Col md='4' xs='4'>
+			<Button disabled={!submitable} color="danger" class="predict" on:click={getPrediction(text)}>predict</Button>
+		</Col>
+	</Row>
+	<Row class="pt-3">
+		<Col md='8' xs='12'>
+			{#if src.prediction.length === 0}
+			<h1>loading...</h1>
+			{:else}
+			<h1>{src.verbose}</h1>
+			{/if}
+		</Col>
+	</Row>
+	<Row>
+		<Col md='8' xs='12'>
+			{#if src.prediction.length === 0}
+			<h3>loading...</h3>
+			{:else}
+			<h3>{src.prediction}</h3>
+			{/if}
+		</Col>
+	</Row>
+<!-- </Form> -->
